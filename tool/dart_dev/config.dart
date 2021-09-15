0730903c4d1d4619aa76b58785bc3991f6d99b64
Anna Watson <anna.watson@workiva.com>
2021-09-15 15:05:43 -0600

    not a workivaConfig

--- tool/dart_dev/config.dart
+++ tool/dart_dev/config.dart
@@ -1,7 +1,5 @@
 import 'package:dart_dev/dart_dev.dart';
-import 'package:dart_dev_workiva/dart_dev_workiva.dart';
 
 final Map<String, DevTool> config = {
-  ...workivaConfig,
   'analyze': AnalyzeTool()..analyzerArgs,
 };
