c5d0e69efa6a2b0967991a3a3949775a26607141
annawatson-wk <50588888+annawatson-wk@users.noreply.github.com>
2021-09-22 14:14:02 -0600

    Update tool/dart_dev/config.dart
    Co-authored-by: Tod Bachman <tod.bachman@workiva.com>

--- tool/dart_dev/config.dart
+++ tool/dart_dev/config.dart
@@ -1,8 +1,6 @@
 import 'package:dart_dev/dart_dev.dart';
 
 final Map<String, DevTool> config = {
-  'analyze': AnalyzeTool()..analyzerArgs,
-  'format': FormatTool(),
-  'test': TestTool(),
+  ...coreConfig,
   'serve': WebdevServeTool()..buildArgs = ['example'],
 };
