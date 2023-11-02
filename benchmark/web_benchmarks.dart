f53233dac39b432cbb0f59a8760b6adaf7705ae9
Travis Sanderson <travis.sanderson@workiva.com>
2023-11-02 16:05:51 -0500

    QOL improvement to run profiler while benchmarks run

--- benchmark/web_benchmarks.dart
+++ benchmark/web_benchmarks.dart
@@ -3,7 +3,16 @@ import 'dart:html';
 import 'benchmarks.dart' as benchmarks;
 
 main() {
-  querySelector('#runButton')!.onClick.listen((_) {
+  final button = querySelector('#runButton')! as ButtonElement;
+  button.onClick.listen((_) async {
+    button.disabled = true;
+    final text = button.innerText;
+    button.innerText = 'Running';
+    await window.animationFrame;
+    await window.animationFrame;
+    await window.animationFrame;
     benchmarks.main();
+    button.disabled = false;
+    button.innerText = text;
   });
 }
